ba8b65213646bbc7858413ce6d93ab64bc193087
tanveer-12 <tanveerkaur1292@outlook.com>
2023-08-09 20:57:07 +0530

    9aug

--- arrays/pairsum.cpp
+++ arrays/pairsum.cpp
@@ -1,1 +1,24 @@
 //WAP to find the total number of pairs in the array whose sum is equal to the given value x
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int arr[] = {3,4,6,7,1};
+    int targetsum = 7;
+
+    int paircount =0;
+    for(int i=0;i<5;i++)
+    {
+        for(int j=i+1;j<5;j++)
+        {
+            if(arr[i]+arr[j]==targetsum)
+            {
+                paircount++;
+            }
+        }
+    }
+    std::cout<<"The number of pairs is: "<<paircount<<std::endl;
+
+    return 0;
+}
