8d884b7d6e7a0f6ed98bf797a62208dae7698a63
tanveer-12 <tanveerkaur1292@outlook.com>
2023-08-12 20:47:19 +0530

    12 aug

--- arrays/Prefix Sum Problems/runningsum.cpp
+++ arrays/Prefix Sum Problems/runningsum.cpp
@@ -2,7 +2,37 @@
 // in the same array without creating a new array
 #include <iostream>
 #include <vector>
+
+void runningsum(std::vector<int> &v);
+
 int main()
 {
-    
+    int n;
+    std::cout<<"Enter arrays size: ";
+    std::cin>>n;
+
+    std::vector<int> v;
+    std::cout<<"Enter array elements: ";
+    for(int i=0; i<n;i++)
+    {
+        int elements;
+        std::cin>>elements;
+        v.push_back(elements);
+    }
+    runningsum(v);
+    for(int i=0;i<n;i++)
+    {
+        std::cout<<v[i]<<" ";
+    }
+    std::cout<<std::endl;
+    return 0;
+}
+
+void runningsum(std::vector<int> &v)
+{
+    for(int i=1; i<v.size();i++)
+    {
+        v[i] = v[i-1] + v[i];      //v[i] += v[i-1]
+    }
+    return;
 }
