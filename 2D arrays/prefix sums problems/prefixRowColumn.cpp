3829ec6a47c3fed8b1a135e9ee90c02ce7327faf
tanveer-12 <tanveerkaur1292@outlook.com>
2023-08-16 21:48:10 +0530

    16 aug

--- 2D arrays/prefix sums problems/prefixRowColumn.cpp
+++ 2D arrays/prefix sums problems/prefixRowColumn.cpp
@@ -1,4 +1,4 @@
-// 2 . pre-calculating the horizontal sum for each row in the matrix
+// 3 . pre-calculating the sum for each row and each column in the matrix
 
 //Given a matrix of 'a' dimension n x m and 2 coordinates  (l1,r1) and (l2,r2).
 //return the sum of rectangle formed from (l1,r1) to (l2,r2)
@@ -45,19 +45,39 @@ int rectangleSum(std::vector<std::vector<int>> &matrix, int l1, int r1, int l2, 
         }
     }
     
-
-    //for returning the sum of the rectangle formed
-    for(int i=l1; i<=l2; i++)
+    //calculating the prefix sum array column-wise
+    for(int i=1; i<matrix.size(); i++)
     {
-        if(r1 != 0)
+        for(int j=0; j<matrix[0].size(); j++)
         {
-            sum += matrix[i][r2] - matrix[i][r1-1];
-        }
-        else
-        {
-            //when r1 = 0 that is matrix[i][j-1] = 0
-            sum += matrix[i][r2];
+            matrix[i][j] += matrix[i-1][j];
         }
     }
+
+    //printing prefix sum 2d array
+    for(int i=0; i<matrix.size(); i++)
+    {
+        for(int j=0; j<matrix[0].size(); j++)
+        {
+            std::cout<<matrix[i][j]<<" ";
+        }
+        std::cout<<std::endl;
+    }
+
+    int top_sum = 0, left_sum = 0, topleft_sum = 0;
+    if(l1 != 0)
+    {
+        top_sum = matrix[l1 - 1][r2];
+    }
+    if(r1 != 0)
+    {
+         left_sum = matrix[l2][r1 - 1];
+    }
+    if(l1!=0 && r1!=0)
+    {
+        topleft_sum = matrix[l1 -1][r1-1];
+    }
+    sum = matrix[l2][r2] - top_sum - left_sum + topleft_sum;
+
     return sum;
 }
